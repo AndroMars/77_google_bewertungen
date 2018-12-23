c26231eb03fef8cc9723307510c1be49eeece828
Sebastian Koch <kochsebastian@gmail.com>
2018-12-23 23:16:26 +0100

    yarn

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from "./App.vue";
 
 Vue.config.productionTip = false;
 
-new Vue({
+let vm = new Vue({
   render: h => h(App)
-}).$mount("#app");
+}).$mount("#mp_rate");
+
+console.log(vm.$data);
