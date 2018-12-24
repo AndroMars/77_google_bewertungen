64fc557d45d21b98ad04e4919081a7f6317f6518
Unknown <marcel2.koch@tu-dortmund.de>
2018-12-24 13:43:14 +0100

    0.3

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <List msg="Welcome to Your Vue.js App" /> -->
+    <ul>
+      <li v-for="item in items" v-bind:key="item">
+        {{ item.author + item.text }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+// import List from "./components/List.vue";
 
 export default {
   name: "app",
-  components: {
-    HelloWorld
-  }
+  data: function() {
+    return {
+      items: [
+        { author: "Name1", text: "irgendwas" },
+        { author: "Name2", text: "Message1" },
+        { author: "irgendwas", text: "Message2" },
+        { author: "Name3", text: "Message3" }
+      ]
+    };
+  },
+  components: {}
 };
 </script>
 
@@ -24,5 +36,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+li {
+  list-style: none;
 }
 </style>
---
+++ src/components/Listitem.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="listitem">
+    <h2>{benutzername}</h2>
+    <p>[message]</p>
+  </div>
+</template>
+<style>
+.listitem {
+}
+</style>
