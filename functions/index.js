3ce055c2c7170edfa0f2a5089a6a8e83cb965bd2
Unknown <marcel2.koch@tu-dortmund.de>
2019-02-08 00:34:32 +0100

    kurz vor 12

--- functions/index.js
+++ functions/index.js
@@ -11,12 +11,42 @@
 const functions = require('firebase-functions');
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
+
+    // google maps api key functions
+
+    const googleMapsClient = require('@google/maps').createClient({
+        key: 'AIzaSyBg5HJZLJHcJozUtQuhNlpqPbK5z744EGA'
+      });
+
+    // Geocode an address. #keine Ahnung was das ist
+
+googleMapsClient.geocode({
+    address: '1600 Amphitheatre Parkway, Mountain View, CA'
+  }, function(err, res) {
+    if (!err) {
+      console.log(res.json.results);
+
+      //AUSGABE
+      response.send(res.json.results);
+    }
+  });
+
+// geocode ende hier
+
+    
 });
 
 // Injoy Stockack Place ID : ChIJv1kVEVpmmkcRRgn5ZOT6PfY
 // Google Place API : AIzaSyBg5HJZLJHcJozUtQuhNlpqPbK5z744EGA
 
-const googleMapsClient = require('@google/maps').createClient({
-    key: 'AIzaSyBg5HJZLJHcJozUtQuhNlpqPbK5z744EGA'
-  });
+
+
+
+
+
+
+
+
+
+
+
